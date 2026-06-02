856c2f291986fec279bad01de733e0e2a5465bf0
jeffersonqpm <jefferson.qpm@gmail.com>
2026-06-01 21:32:43 -0300

    Terceiro exemplo

--- Aula-02/src/App.jsx
+++ Aula-02/src/App.jsx
@@ -8,25 +8,57 @@ import { useState } from "react";
 
 function App() {
 
+  const [nome, setNome] =  useState("Jefferson");
+  const [descricao, setDescricao] =  useState("");
+  const [status, setStatus] =  useState("");
+
+  const tarefa = {
+    nome: nome,
+    descricao: descricao,
+    status: status
+
+  }
+  
+  salvar9(tarefas);
+  // ===== exemplo 03=====================
+
+
+  // const [nome, setNome] = useState("Jefferson");
+
+  // const element = (e) => {
+  //   setNome(e.target.value);
+  // console.log(e);
+  // }
+
+  // return(
+  //   <form action="">
+  //     <label htmlFor="">Digite seu nome: </label>
+  //     <input type="text"  onChange={(e) => setNome(e.target.value)}/>
+  //     {nome}
+  //   </form>
+  // );
+
+  // ======================= fim exemplo 3
+
   // Exemplo 02
 
-  const [valorAtual, ValorAlterado] = useState(1);
+  // const [valorAtual, ValorAlterado] = useState(1);
 
-  const increment = () => {
-    ValorAlterado(valorAtual + 1)
-  }
+  // const increment = () => {
+  //   ValorAlterado(valorAtual + 1)
+  // }
 
-    const decrement = () => {
-    ValorAlterado(valorAtual - 1)
-  }
+  //   const decrement = () => {
+  //   ValorAlterado(valorAtual - 1)
+  // }
 
 
-  return <div>
+  // return <div>
 
-    {valorAtual}
-    <button onClick={increment}>Aumentar</button>
-    <button onClick={decrement}>Diminuir</button>
-  </div>
+  //   {valorAtual}
+  //   <button onClick={increment}>Aumentar</button>
+  //   <button onClick={decrement}>Diminuir</button>
+  // </div>
 
   // ================================= fim do exemplo 02
 
