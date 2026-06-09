3c2a6648fba30f0060fd403393bce24f98ee42e1
jeffersonqpm <jefferson.qpm@gmail.com>
2026-06-08 21:44:39 -0300

    feat(usuarios): buscar dados da API e renderizar lista de usuários

--- Aula-04/src/Pages/Usuarios.jsx
+++ Aula-04/src/Pages/Usuarios.jsx
@@ -1,28 +1,76 @@
 import { useEffect, useState } from "react";
+import { data } from "react-router-dom";
+import axios from "axios";
 
 
 function Usuarios() {
 
-    const [numero, setNumero] = useState(0);
-
-    const increment = () => {
-        // alert('OK');
-        setNumero(numero + 1);
-        // alert(numero);
-    }
-    // console.log(Olá);
+    const [usuario, setUsuario] = useState([]);
 
     useEffect(() => {
-        console.log("Olá");
+
+        axios.get("https://jsonplaceholder.typicode.com/users")
+            .then((resp) => setUsuario(resp.data));
+
     },[]);
-    // },[numero]);
-    return <div>
 
-        <h2>Tela de Usuarios</h2>
 
-        <button onClick={increment}>Clique aqui!</button>
 
-    </div>
+
+return <div>
+
+    <h2>Tela de Usuarios</h2>
+
+    <ul>
+        {usuario.map((usuario) => (<li key={usuario.id}> {usuario.name} - {usuario.email}</li>))}
+    </ul>
+
+</div>
+    // ==============exemplo 02=====================
+
+    // useEffect(() => {
+
+    //     fetch("https://jsonplaceholder.typicode.com/users")
+    //     .then((resp)=> resp.json())
+    //     .then((data) =>  {setUsuario(data);
+
+    //     })
+
+    // }, []);
+
+    // return <div>
+
+    //     <h2>Tela de Usuarios</h2>
+
+    //     <ul>
+    //         {usuario.map((usuario)=> (<li  key={usuario.id}> {usuario.name} - {usuario.email}</li>))}
+    //     </ul>
+
+    // </div>
+
+
+    // ===============exemplo 01============================
+
+    // const [numero, setNumero] = useState(0);
+
+    // const increment = () => {
+    //     // alert('OK');
+    //     setNumero(numero + 1);
+    //     // alert(numero);
+    // }
+    // // console.log(Olá);
+
+    // useEffect(() => {
+    //     console.log("Olá");
+    // },[]);
+    // // },[numero]);
+    // return <div>
+
+    //     <h2>Tela de Usuarios</h2>
+
+    //     <button onClick={increment}>Clique aqui!</button>
+
+    // </div>
 }
 
 export default Usuarios
